f9c59e99b070d259e893ecde47acc4bde4406414
Artem Golovin <artgolovin96@gmail.com>
2019-12-01 15:58:20 -0700

    improvement: add better cli args handling using yargs

---
+++ src/solve.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const yargs = require('yargs');
+
+const argv = yargs
+  .usage('$0 [args]')
+  .option('day', {
+    alias: 'd',
+    number: true,
+    description: 'Run solution for specified day',
+  })
+  .option('part', {
+    alias: 'p',
+    number: true,
+    description: 'Specify part for the problem of the day',
+    implies: 'day',
+  })
+  .option('all', {
+    alias: 'A',
+    boolean: true,
+    description: 'Run all solutions',
+  })
+  .help().argv;
+
+const {day, part} = argv;
+
+const getInput = day =>
+  fs
+    .readFileSync(`${__dirname}/day${day}/input${day}`, 'utf-8')
+    .split('\n')
+    .map(n => Number(n));
+
+const solution = require(`./day${day}`);
+
+console.log(`solution for day ${day} = ${solution(getInput(day))}`);
