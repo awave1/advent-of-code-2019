67ce1ba54f6c9262e402746110c52d1469b30760
Artem Golovin <artgolovin96@gmail.com>
2019-12-30 15:00:31 -0700

    :sparkles: day3 p1 solved

--- src/day3/day3.p1.js
+++ src/day3/day3.p1.js
@@ -1,16 +1,63 @@
-const parse = data => data.split('\n').map(line => line.split(','));
+const _ = require('lodash');
+const { abs } = Math;
+
+const parse = data =>
+  data.split('\n').map(line =>
+    line.split(',').map(v => ({
+      direction: v.substring(0, 1),
+      val: Number.parseInt(v.substring(1), 10),
+    }))
+  );
+
+const dx = { R: 1, L: -1, U: 0, D: 0 };
+const dy = { U: 1, D: -1, L: 0, R: 0 };
 
 /**
- * - Find all intersections
- * - Find distances from origin to each intersection (manhattan distance)
- * - get the shortest
  *
- * @param {*} input
+ * @param {string[]} wire
  */
+function plotPoints(wire) {
+  let x = 0;
+  let y = 0;
+  const set = new Set();
+
+  set.add({ x, y });
+
+  for (const { direction, val } of wire) {
+    for (let i = 0; i < val; i++) {
+      x += dx[direction];
+      y += dy[direction];
+
+      set.add({ x, y });
+    }
+  }
+
+  return [...set];
+}
+
+function intersection(pointsA, pointsB) {
+  const result = [];
+  for (const { x: xA, y: yA } of pointsA) {
+    for (const { x: xB, y: yB } of pointsB) {
+      if (xA === xB && yA === yB) {
+        result.push({ x: xA, y: yB });
+      }
+    }
+  }
+
+  return result;
+}
 
 function solve(input) {
-  const parsed = parse(input);
-  console.log(parsed);
+  const [wireA, wireB] = parse(input);
+  const pointsA = plotPoints(wireA);
+  const pointsB = plotPoints(wireB);
+  const intersected = intersection(pointsA, pointsB);
+  const sums = intersected
+    .map(({ x, y }) => abs(x) + abs(y))
+    .filter(sum => sum != 0);
+
+  return _.min(sums);
 }
 
 module.exports = {
--- src/day3/day3.p1.test.js
+++ src/day3/day3.p1.test.js
@@ -3,7 +3,13 @@ const { solve } = require('./day3.p1');
 test('day3', () => {
   let input = `R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83`;
   let expected = 159;
-
   let result = solve(input);
-  expect(true).toBe(true);
+
+  expect(result).toBe(expected);
+
+  input = `R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7`;
+  expected = 135;
+  result = solve(input);
+
+  expect(result).toBe(expected);
 });
